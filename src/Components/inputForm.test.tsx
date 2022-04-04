8bf6dc155a7d8b24e1fbaceaa47d73bd5c43b128
bilalAhmad69 <bilal.khattak69@gmail.com>
2022-04-04 15:16:45 +0500

    refine code

--- src/Components/inputForm.test.tsx
+++ src/Components/inputForm.test.tsx
@@ -1,40 +1,37 @@
 import {render , fireEvent} from "@testing-library/react";
-import { BrowserRouter , Route, Routes} from "react-router-dom";
+import { BrowserRouter , Route, Routes , MemoryRouter} from "react-router-dom";
 import {InputForm} from "./inputForm";
 // is Component Render
 describe("InputForm component" , ()=>{
     it ("button and text Field Should be Rendered" , ()=>{
         const {getByTestId} = render(
-        <BrowserRouter>
-        <Routes>   
-     <Route path="/" element={<InputForm/>}/>  
-        </Routes>
-       </BrowserRouter>
+        <MemoryRouter>
+         <InputForm/>
+         </MemoryRouter> 
+       
         )
-        const inputTxt = getByTestId("inputTxt");
+        const inputTxt = getByTestId("inputTxt") as HTMLInputElement;
         const btn = getByTestId("btn");
-        
         expect(inputTxt).toBeTruthy();
         expect(btn).toBeTruthy();
+        fireEvent.change(inputTxt, {
+            target: { value: "testing" }
+          });
+        expect(inputTxt.value).toBe("testing")
     })
-})
-// test for onChange 
-describe("check the Value of Input field On Change" , ()=>{
-    it("check The On change value" , ()=>{
-       const {getByTestId} =   render(
-            <BrowserRouter>
-            <Routes>   
-         <Route path="/" element={<InputForm/>}/>  
-            </Routes>
-           </BrowserRouter>
-       );
-       const inputTxt = getByTestId("inputTxt")as HTMLInputElement;
-       fireEvent.change(inputTxt, {
-        target: { value: "testing" }
-      });;
-       expect(inputTxt.value).toBe("testing")
-    })
-})
-
-
+  it ("check the value Of TextField on OnChange" , ()=>{
+    const {getByTestId} = render(
+        <MemoryRouter>
+         <InputForm/>
+         </MemoryRouter> 
+       
+        )
+        const inputTxt = getByTestId("inputTxt") as HTMLInputElement;
+    fireEvent.change(inputTxt, {
+                target: { value: "testing" }
+              });;
+               expect(inputTxt.value).toBe("testing")
+  })
+  
 // test Case for onClick to change url
+})
